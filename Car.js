ce2a61201f83de85144c2a7efb9a8ba0f8aff1ec
viranjali <76208580+viranjali@users.noreply.github.com>
2024-04-13 18:25:48 +0530

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent("car-model",{
+    schema:{
+        modelRef: { type: "string", default: "../assets/car/scene.gltf" }
+    },
+    init:function(){
+    this.el.setAttribute("gltf-model", this.data.modelRef);
+    const position = { x: 0, y: 50, z: 80 };
+    const rotation = { x: 0, y: -100, z: 0 };
+    this.el.setAttribute("position", position);
+    this.el.setAttribute("rotation", rotation);
+    },
+    update:function(){
+        window.addEventListener("click",(e)=>{
+            this.data.clickCounter=this.data.clickCounter + 1;
+            if(this.data.clickCounter === 1){
+                const rotation = {x:0,y:20,z:0};
+                this.el.setAttribute("rotation",rotation);
+            }
+        })
+    }
+})
